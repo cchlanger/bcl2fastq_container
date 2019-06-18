5045d45d117ede151cfa8ff66a2e90a742d7f3c4
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2019-06-18 08:18:01 +0200

    Initial version

---
+++ bcl2fastq/add_conda_rpm.sh
@@ -0,0 +1,8 @@
+cat <<EOF > /etc/yum.repos.d/conda.repo
+[conda]
+name=Conda
+baseurl=https://repo.anaconda.com/pkgs/misc/rpmrepo/conda
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc
+EOF
